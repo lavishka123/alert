3b099b676ad858d8b975e17acabc76a548cd2115
lavishka123 <lavishkakumari0903@gmail.com>
2024-07-19 07:19:07 +0500

    alert assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+alert("welcome to our website")  // NUMBER 01
+
+var name = prompt("ENTER YOUR NAME")
+alert(" Welcome to our website " +  name   + " ! ")
+
+alert(" Error ! " + "Please enter a valid password") //NUMBER 02
+
+
+
+alert("Welcome to JS land \nHappy coding !")  //NUMBER 03
+
+
+alert("Welcome to JS Land....") //NUMBER 04
+alert("Happy Coding ! \nPrevent this page from creating additional dialogs ") //NUMBER 04
+
+
+
+alert("Hello ..... I can run JS through my web browser`s console") //NUMBER 05
